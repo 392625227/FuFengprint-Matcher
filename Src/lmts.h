5797a738746c638fa894a7cc7567541f1aa20e0a
392625227 <40751795+392625227@users.noreply.github.com>
2019-04-05 16:58:36 +0800

    Update lmts.h
    good format for google translate

--- Src/lmts.h
+++ Src/lmts.h
@@ -7,8 +7,7 @@
 #include "template.h"
 
 /*
- * Local minutia topologic structure = neighbouring minutia within a given
- * distance R of a central minutia.
+ * Local minutia topologic structure = neighbouring minutia within a given distance R of a central minutia.
  *
  * Each neighbouring minutia m, forming a vector v with the central minutia c
  * is defined by 3 coordinates:
@@ -16,20 +15,14 @@
  * - the angle between v and the orientation of c (angular coordinate)
  * - the angle between the orientation of m and v(orientation coordinate)
  *
- * To make computations on angles easier and more efficients, we use
- * the wrap around behaviour on C unsigned chars.
- * Thus, angles in [0, 2pi[ are mapped over [0, 256[
+ * To make computations on angles easier and more efficients, we use the wrap around behaviour on C unsigned chars.
+ Thus, angles in [0, 2pi[ are mapped over [0, 256[
  *
- * Also, in order to have equal weights for each of the coordinate when
- * computing the L1 distance between 2 neighbouring minutia, we actually
- * use the ratio (length of v / R) mapped over [0, 128[ as radial coordinate
+ * Also, in order to have equal weights for each of the coordinate when computing the L1 distance between 2 neighbouring minutia, we actually use the ratio (length of v / R) mapped over [0, 128[ as radial coordinate
  *
- * This way, the greatest distance between two radial coordinates or two
- * angular coordinates or two orientation coordinates is 128.
- * (The L1 distance between 2 neighbouring minutia is thus in [0, 384]).
+ * This way, the greatest distance between two radial coordinates or two angular coordinates or two orientation coordinates is 128. (The L1 distance between 2 neighbouring minutia is thus in [0, 384]).
  *
- * Note : the coordinates of a neighbouring minutia are not sensible to
- * translations or rotations of the fingerprint
+ * Note : the coordinates of a neighbouring minutia are not sensible to translations or rotations of the fingerprint
  *
  */
 typedef struct LocalMinutiaTopologicStructure
@@ -41,21 +34,18 @@ typedef struct LocalMinutiaTopologicStructure
 } LMTS;
 
 /*
- * Computes an array of all the LMTS of radius `r` (in cm) in
- * template `t`, using the specified pre computed distance matrix (in cm)
- * `distances` and nb nbNeighbours array `nbNeighbours`.
- * Stores it in `lmts`
+  Computes an array of all the LMTS of radius `r` (in cm) in template `t`, using the specified pre computed distance matrix (in cm) `distances` and nb nbNeighbours array `nbNeighbours`. Stores it in `lmts`
  */
 void LMTS_buildAll(LMTS *lmts, T* t, float r, float *distances, int *nbNeighbours);
 
 /**
- * Releases ressources associated with the n first LMTS of the specified
- * LMTS array
+ Releases ressources associated with the n first LMTS of the specified
+ LMTS array
  */
 void LMTS_free(int n, LMTS *lmts);
 
 /**
- * Prints the specified lmts (if it is not NULL) on the specified stream
+ Prints the specified lmts (if it is not NULL) on the specified stream
  */
 void LMTS_print(FILE *stream, LMTS *lmts);
 
